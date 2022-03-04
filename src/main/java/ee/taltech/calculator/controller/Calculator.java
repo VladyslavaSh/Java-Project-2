436a9b6a915e3e68cf1500a4a39e06024c3e9e02
Vladyslava Shekula <shekulavlada@gmail.com>
2022-03-04 20:36:08 +0200

    Write calculate1 method (operation MinOdd)

--- src/main/java/ee/taltech/calculator/controller/Calculator.java
+++ src/main/java/ee/taltech/calculator/controller/Calculator.java
@@ -1,7 +1,11 @@
 package ee.taltech.calculator.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class Calculator {
@@ -11,4 +15,13 @@ public class Calculator {
         return "Hello!";
     }
 
+    @GetMapping("calculate1")
+    public void calculate1(@RequestParam List<Integer> input){
+
+        Optional<Integer> MinOdd = input.stream()
+                .filter(x -> x % 2 != 0)
+                .min(Integer::compare);
+        
+    }
 }
+
