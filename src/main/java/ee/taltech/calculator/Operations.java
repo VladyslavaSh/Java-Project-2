4a854bc1e07b8e2e4f1343f3b57a763b6163c09b
Aleksandrs Rimlins <alriml@ttu.ee>
2022-03-07 10:23:40 +0200

    Added null returns where applicable

--- src/main/java/ee/taltech/calculator/Operations.java
+++ src/main/java/ee/taltech/calculator/Operations.java
@@ -7,21 +7,32 @@ import java.math.RoundingMode;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class Operations {
 
+    private static Stream<Integer> getEvenStream(List<Integer> numbers) {
+        return numbers.stream().filter(x -> x % 2 == 0);
+    }
+
+    private static Stream<Integer> getPositivesStream(List<Integer> numbers) {
+        return numbers.stream().filter(x -> x >= 0);
+    }
 
     public static Optional<Integer> minOdd(List<Integer> input) {
         return input.stream().filter(x -> x % 2 != 0).min(Integer::compare);
     }
 
     public static Integer sumOfEven(List<Integer> input) {
-        return input.stream().filter(x -> x % 2 == 0).mapToInt(Integer::intValue).sum();
+        if (getEvenStream(input).collect(Collectors.toList()).isEmpty()) { return null; }
+        return getEvenStream(input).mapToInt(Integer::intValue).sum();
     }
 
     public static List<Integer> odds(List<Integer> input) {
-        return input.stream().filter(x -> x % 2 != 0).collect(Collectors.toList());
+        List<Integer> output = input.stream().filter(x -> x % 2 != 0).collect(Collectors.toList());
+        if (output.isEmpty()) { return null; }
+        return output;
     }
 
     public static List<Integer> squared(List<Integer> input) {
@@ -29,7 +40,8 @@ public class Operations {
     }
 
     public static BigDecimal averageOfPositives(List<Integer> input) {
-        double avg = input.stream().filter(x -> x >= 0).mapToDouble(x -> (double) x).average().orElse(0.0);
+        if (getPositivesStream(input).collect(Collectors.toList()).isEmpty()) { return null; }
+        double avg = getPositivesStream(input).mapToDouble(x -> (double) x).average().orElse(0.0);
         return new BigDecimal(avg).setScale(2, RoundingMode.HALF_UP);
     }
 }
