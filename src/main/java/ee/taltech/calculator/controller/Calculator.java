6893ea8c0ed259047c7b3f30a7fe347c6bda4dff
Aleksandrs Rimlins <alriml@ttu.ee>
2022-03-08 21:18:58 +0200

    Changed mapping (/calculator/calculate<1-2> instead of /calculate<1-2>)

--- src/main/java/ee/taltech/calculator/controller/Calculator.java
+++ src/main/java/ee/taltech/calculator/controller/Calculator.java
@@ -3,12 +3,14 @@ package ee.taltech.calculator.controller;
 import ee.taltech.calculator.calculation.Calculation1;
 import ee.taltech.calculator.calculation.Calculation2;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("calculator")
 public class Calculator {
 
     @GetMapping("calculate1")
